1ab8c38f82d1cdc880fe3429813d8b7441e80b42
Caio-Marinho <kaigabriel12@gmail.com>
2026-08-02 13:29:11 -0300

    criando erro de telefone e e-mail para dados em branco

--- app/dto/globals/emailItemDTO.py
+++ app/dto/globals/emailItemDTO.py
@@ -104,12 +104,16 @@ class EmailItem(BaseModel):
         # Converte o dado de entrada para string padrão e remove espaços invisíveis das bordas
         email_str = str(email).strip() if email else ""
 
+        # verifica se foi passado uma string valida se não bloqueia
+        if email_str == "":
+            raise ValueError("O email está em branco")
+
         # Aciona o motor regex auxiliar para verificar se o e-mail possui corpo e formato válidos
         if validar_email(email_str):
             return email_str
 
         # Lança exceção capturada nativamente pelo Pydantic caso falhe no regex ou este esteja em branco
-        raise ValueError("O email não é um e-mail válido ou está em branco")
+        raise ValueError("O email não é um e-mail válido")
 
 
 # =================================================================
